d3cf39955e818850a6a3ef760988c5e80cc03dc6
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2020-01-07 17:05:38 -0500

    one more typo

--- explanation.py
+++ explanation.py
@@ -33,7 +33,7 @@ This simulation illustrates the behavior of Polya's Urn.
 the initial numbers of white and black balls are a and b respectively, then this histogram converges to (a good approximation) of
 the Beta(a,b) distribution, appropriately rescaled.
 </ul>
-<a href="https://gihub.com/jeremy9959/Polya.git"> The code that generates this app is here.</a>
+<a href="https://github.com/jeremy9959/Polya.git"> The code that generates this app is here.</a>
 <br><br>
 To use the simulation, choose your initial number of black and white balls and push Go!
 """
